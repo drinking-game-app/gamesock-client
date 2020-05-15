caccaa42769d6c2badc0752f0548f18aa1f9249f
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-05-15 22:05:00 +0100

    Updated readme and added Jdocs to default functions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
 import ioClient from 'socket.io-client';
 
-const setup = ()=>{
+let url=`http://localhost:3000`;
+const setup = (endpointURL:string)=>{
+    url=endpointURL;
+}
+
+const connect = ()=>{
     // Setup
-    return ioClient.connect(`http://localhost:8000`, {
+    return ioClient.connect(url, {
     transports: ['polling', 'websocket'],
   });
   }
 
+/**
+ *  JoinLobby will join a currently open lobby
+ * @param lobbyName The name of the lobby you want to join as a string
+ */
+const joinLobby = (lobbyName:string) => {
+    const clientSocket = connect();
+    clientSocket.emit('joinLobby', lobbyName);
+}
 
-const join = (lobbyName:string)=>{
-    const clientSocket = setup();
-    clientSocket.emit('createLobby', 'test');
+/**
+ *  Create lobby will create a lobby if the user is authorized
+ * @param lobbyName The name of the lobby you want to join as a string
+ * @param authToken The auth token to verify the user
+ */
+const createLobby = (lobbyName:string, authToken:string) => {
+    const clientSocket = connect();
+    clientSocket.emit('createLobby', lobbyName, authToken);
 }
 
 export default{
-    join
+    setup,
+    joinLobby,
+    createLobby
 }
